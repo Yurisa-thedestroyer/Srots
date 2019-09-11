1d905b9930321bbe53608456dcfc74a647d42173
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2019-09-11 09:54:17 -0300

    Add files via upload

---
+++ Merg_B/Merg.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int PalCount(v)
+{
+	int c = 0; 
+	
+	return c;
+}
+
+void Merge()
+{
+	
+}
+
+int MergSort(v[], int c, int f)
+{
+	int m = (c+f)/2;
+	
+	MergSort(v[], c,  m);
+	mergsort(v[], m +1 , f)
+	
+	return 0;
+}
+
+int main()
+{
+	
+	return 0;
+}
