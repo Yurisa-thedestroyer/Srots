ba730f651473ccc7acce56a917abd9eacf9a6aab
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2019-09-11 22:11:43 -0300

    Update Merg.c

--- Merg_B/Merg.c
+++ Merg_B/Merg.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h>>
 #include <string.h>
 #include <ctype.h>
 
@@ -12,16 +12,23 @@ int PalCount(v)
 
 void Merge(char v[], int c,int m, int f)
 {
+	int x,y,z;
+	
+
+	for(int i = 0;i<= ;i++);
+	{
+			
+	}
 	
 }
 
-int MergSort(v[], int c, int f)
+int MergSort(char v[], int c, int f)
 {
 	int m = (c+f)/2;
 	
 	MergSort(v[], c,  m);
 	Mergsort(v[], m +1 , f)
-	Merge();
+	Merge(v, l,m,f);
 	
 	return 0;
 }
