3cf5d282c00e4b98d9fb6e2ba4be5480876777a8
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2019-09-22 13:49:37 -0300

    Update Merg.c

--- Merg_B/Merg.c
+++ Merg_B/Merg.c
@@ -12,13 +12,52 @@ int PalCount(v)
 
 void Merge(char v[], int c,int m, int f)
 {
-	int x,y,z;
+    int i, j, k; 
+    int n1 = m - c + 1; 
+    int n2 =  f - m; 
+  
+    
+    int L[n1], R[n2]; 
+  
+    
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[c + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    i = 0; 
+    j = 0;  
+    k = c;  
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
 	
-
-	for(int i = 0;i<= ;i++);
-	{
-			
-	}
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
 	
 }
 
